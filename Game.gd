adfb778fc50b305b5a5d7aa3aac52e7a9d839df7
whoji <minli1985@gmail.com>
2020-03-31 20:28:19 -0700

    made player movement constrained in the board area.

---
+++ Game.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const MAP_SIZE = Vector2(4,4)
+const MAP_OFFSET = Vector2(3,2)
+onready var map = []
+onready var player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(MAP_SIZE.x):
+		map.append([])
+		for j in range(MAP_SIZE.y):
+			map[i].append(0)
+
+	map[player.grid_position.x][player.grid_position.y] = 1
+
+	print(map)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Player.gd
+++ Player.gd
@@ -7,11 +7,12 @@ const TILE_SIZE = 32
 var grid_position = Vector2(0,0)
 
 onready var tween = get_node("Tween")
-
+onready var game = get_tree().get_root().get_node("Game")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	grid_position = position / TILE_SIZE
+	# grid_position = position / TILE_SIZE
+	pass
 
 func _input(event):
 	if !event.is_pressed(): 
@@ -26,14 +27,32 @@ func _input(event):
 		move_grid(0, 1)
 
 func move_grid(dx, dy):
+	
+	if not check_move_valid(dx, dy):
+		return
+	
 	grid_position.x += dx 
 	grid_position.y += dy
 	
+	var dest_position = (grid_position + game.MAP_OFFSET) * TILE_SIZE
+	
 	tween.interpolate_property(self, "position",
-		position, grid_position * TILE_SIZE, 0.1,
+		position, dest_position, 0.1,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 	
 	position = grid_position * TILE_SIZE
 
+func check_move_valid(dx, dy):
+	var _dest_grid_position = grid_position + Vector2(dx, dy)
+	if _dest_grid_position.x < 0:
+		return false
+	elif _dest_grid_position.y < 0:
+		return false
+	elif _dest_grid_position.x >= game.MAP_SIZE.x:
+		return false
+	elif _dest_grid_position.y >= game.MAP_SIZE.y:
+		return false
+	else:
+		return true
 
