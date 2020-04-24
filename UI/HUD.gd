59d11947bb3a2c3f130758c42652feb358406c57
whoji <minli1985@gmail.com>
2020-04-23 22:34:50 -0700

    fixed the HUD display

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -9,8 +9,8 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$LevelLabel.text = "Level: "+str(Global.current_level)
-	$GoldLabel.text = "Level: "+str(Global.gold)
-	$ExpLabel.text = "Level: "+str(Global.exp_)
+	$GoldLabel.text = "Gold: "+str(Global.gold)
+	$ExpLabel.text = "Exp: "+str(Global.exp_)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
