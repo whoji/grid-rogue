cb80ccc0d12db3462f48a5c25b1b2ace1ae5f001
whoji <minli1985@gmail.com>
2020-04-27 23:42:59 -0700

    added this very simple IDLE for test basics

---
+++ misc/SimpleTest.gd
@@ -0,0 +1,18 @@
+extends Node
+
+# this is my IDLE
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var a = [1, 2 ,3 ]
+	print(a) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
