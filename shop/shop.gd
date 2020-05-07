94244a9409067d423c35d3f783bc178b38c6b410
whoji <minli1985@gmail.com>
2020-05-06 22:31:45 -0700

    added a pop-up dialog win to tell the play that he/she is too poor.

--- shop/shop.gd
+++ shop/shop.gd
@@ -5,6 +5,7 @@ const TILE_OFFSET = Vector2(1,1)
 onready var selector = $SelectorRect
 onready var selector_gpos = Vector2(1,1)
 onready var tween = get_node("Tween")
+onready var flag_if_dialog_on = false
 
 func _ready():
 	pass # Replace with function body.
@@ -20,6 +21,12 @@ func _input(event):
 		move_grid(0, -1)
 	elif event.is_action("ui_down"):
 		move_grid(0, 1)
+	elif event.is_action("ui_select"):
+		if flag_if_dialog_on:
+			$PopupDialog/ColorRect.visible = false
+			flag_if_dialog_on = false	
+		else:
+			buy_hero()
 		
 func move_grid(dx, dy):
 	selector_gpos.x += dx 
@@ -31,4 +38,15 @@ func move_grid(dx, dy):
 	tween.start()
 	selector.rect_position = selector_gpos * TILE_SIZE
 	
-		
+func buy_hero():
+	var hero_id = get_hero_id(selector_gpos)
+#	if Global.gold >= Conf.heroes[hero_id]["gold"]:
+#		pass
+#	else:
+#		print("YOU CANNOT AFFORD BUY THIS THING !!!...")
+#		$PopupDialog/ColorRect.visible = true
+	$PopupDialog/ColorRect.visible = true
+	flag_if_dialog_on = true
+	
+func get_hero_id(selector_gpos):
+	pass
