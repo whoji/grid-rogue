3be954727b2cafa4e0dfe6994990085420323688
whoji <minli1985@gmail.com>
2020-05-07 21:51:23 -0700

    added a title screen

---
+++ UI/TitleScreen.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+const all_cursor_pos = [Vector2(32, 96),Vector2(32, 128),Vector2(32, 160),Vector2(32, 192)]
+onready var cursor_pos_idx = 0
+onready var rect = $Rect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if !event.is_pressed(): 
+		return
+	elif event.is_action("ui_up"):
+		cursor_pos_idx -= 1
+		cursor_pos_idx = max(cursor_pos_idx, 0)
+		move_rect()
+	elif event.is_action("ui_down"):
+		cursor_pos_idx += 1
+		cursor_pos_idx = min(cursor_pos_idx, 3)
+		move_rect()		
+	elif event.is_action("ui_select"):
+		if cursor_pos_idx == 3:
+			get_tree().quit()
+#		if flag_if_dialog_on:
+#			$PopupDialog/ColorRect.visible = false
+#			flag_if_dialog_on = false	
+#		else:
+#			buy_hero()
+
+func move_rect():
+	print(cursor_pos_idx)
+	rect.rect_position = all_cursor_pos[cursor_pos_idx] + Vector2(10, 0)
